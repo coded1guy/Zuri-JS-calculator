47cd1d9bcb215667cf6bcbb9efb37fa02348bb6e
coded1guy <grggmmnl@gmail.com>
2022-06-22 20:12:59 +0100

    update

---
+++ script.js
@@ -0,0 +1,42 @@
+const validateOperand = (num1, op, num2) => {
+    console.log("entered here");
+    let sol;
+    switch(op) {
+        case "+":
+            sol = num1 + num2;
+            break;
+        case "-":
+            sol = num1 - num2;
+            break;
+        case "*":
+            sol = num1 * num2;
+            break;
+        case "/":
+            sol = num1 / num2;
+            break;
+        case "%":
+            sol = num1 % num2;
+            break;
+        default:
+            sol =  "invalid";
+    }
+    return sol;
+}
+const start = () => {
+    alert("This is a JavaScript basic Calculator");
+    num1 = prompt("Input the first number");
+    operator = prompt("Input the basic operator(it should be one of these): +, -, *, /, %");
+    num2 = prompt("Input the second number");
+    console.log(num1, operator, num2);
+    if(typeof num1 == 'number' && typeof num2 == 'number') {
+        console.log("pass1");
+        let sol = validateOperand(num1, operator, num2);
+        if(sol == "invalid") {
+            alert("invalid operation");
+            start();
+        } else {
+            alert("answer= " + sol);
+        }
+    }
+}
+start();
